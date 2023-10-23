f1f7fb9c5a017c8ff28b72ecca82d45da8215063
saymanamee <144763250+saymanamee@users.noreply.github.com>
2023-10-23 10:08:24 +0300

    katalog C++

---
+++ katalog/katalog.cpp
@@ -0,0 +1,58 @@
+// katalog.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+#include "stdlib.h"
+#include "string"
+#include "dirent.h"
+
+using namespace std;
+
+void processDirectory(const std::string& directoryPath) 
+{ 
+    DIR* dir = opendir(directoryPath.c_str());
+    if (dir == nullptr) 
+	{
+        std::cerr << "Failed to open directory: " << directoryPath << std::endl;
+        return;
+    }
+
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != nullptr) 
+	{
+        std::string name = entry->d_name;
+        if (name == "." || name == "..") 
+		{
+            continue;
+        } 
+        std::string fullPath = directoryPath + "/" + name;
+
+        struct stat fileInfo;
+        if (stat(fullPath.c_str(), &fileInfo) == -1) 
+		{
+            std::cerr << "Failed to get file info: " << fullPath << std::endl;
+            continue;
+        }
+        
+        if (S_ISDIR(fileInfo.st_mode)) 
+		{
+            processDirectory(fullPath);
+        } else if (S_ISREG(fileInfo.st_mode)) 
+		{
+            std::cout << fullPath << std::endl;
+        }
+    }
+
+    closedir(dir);
+}
+
+int main() 
+{
+    std::string directoryPath = "D:\DOS";
+    processDirectory(directoryPath);
+
+    return 0;
+}
+	
